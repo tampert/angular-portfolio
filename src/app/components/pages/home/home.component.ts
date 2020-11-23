111e9ccb2a1ac64bd2d3917352c2d937e8bc7adc
tampert <d_soentjens@hotmail.com>
2020-11-23 15:10:31 +0100

    adding home page data

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  geboren:Date = new Date('10/20/1978');
+  nu:Date = new Date();
+  dif:number = this.nu.getTime() - this.geboren.getTime();
+  difdays:number = this.dif/(1000 * 3600 * 24);
+  difYears:number = Math.floor(this.difdays/365);
 
   constructor() { }
 
