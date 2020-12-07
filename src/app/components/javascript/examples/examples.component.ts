2cc0ed7a233c8c869a63073006d20fededd2d404
tampert <d_soentjens@hotmail.com>
2020-12-07 09:35:49 +0100

    added some javascript examples

--- src/app/components/javascript/examples/examples.component.ts
+++ src/app/components/javascript/examples/examples.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-examples',
@@ -6,8 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./examples.component.css']
 })
 export class ExamplesComponent implements OnInit {
+  codeFind: SafeHtml;
+  
+  constructor(private sanitized: DomSanitizer) { 
+    this.codeFind =`
+    const fruits = [
+      {
+        id:"1", 
+        name:"apple"
+      },
+      {
+        id:"2", 
+        name:"pear"
+      },
+      {
+        id:"3", 
+        name:"lemon"
+      }
+    ]
 
-  constructor() { }
+    function search(keyword){
+      const results = fruits.filter((fruit) => {
+        return fruit.name.toLocaleLowerCase().includes(keyword)
+      })
+    }
+    `
+  }
 
   ngOnInit(): void {
   }
